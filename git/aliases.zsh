d58afaa6d6628fcb2ee63fe3bb02c66b1f213fdb
colinross <colinross@gmail.com>
2016-06-22 13:37:55 -0700

    Updates

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -12,9 +12,15 @@ alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
+alias gci=gc
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
+alias g="git"
+alias g-="git rm"
+alias g+="git add"
+alias gst="git status"
+alias gdiff="git diff HEAD"
--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,4 +1,6 @@
 # init according to man page
+export RBENV_ROOT="/usr/local/Cellar/rbenv/1.0.0"
+export RBENV_VERSION="2.3.1"
 if (( $+commands[rbenv] ))
 then
   eval "$(rbenv init -)"
